2b01d9f4ea017c855ee8ab11cc90a6c3afc17e7a
Ricardo Maya <rickmaya@gmail.com>
2021-06-23 23:28:24 -0500

    feat: create fetch/update routes and controllers

---
+++ controllers/locationController.js
@@ -0,0 +1,55 @@
+const Location = require('../models/location');
+const superagent = require('superagent');
+
+// fetches all locations
+exports.fetchLocations = (req, res) => {
+	// search for locations by category
+	let conditions = {};
+	if (req.query.category) {
+		conditions.category = req.query.category;
+	}
+	Location.find(conditions, (err, locations) => {
+		if (err) {
+			return res.status(500).json({message: err});
+		} else {
+			return res.status(200).json({locations});
+		}
+	})
+}
+
+// fetches single location
+exports.fetchSingleLocation = (req, res) => {
+	Location.findOne({_id: req.params.id}, (err, location) => {
+		if (err) {
+			return res.status(500).json({message: err});
+		} else if (!location) {
+			return res.status(404).json({message: "location not found"});
+		} else {
+			return res.status(200).json({location});
+		}
+	})
+}
+
+// update single location
+exports.updateSingleLocation = (req, res) => {
+	Location.findByIdAndUpdate(req.params.id, {
+		location: req.body.location,
+		title: req.body.title,
+		comments: req.body.comments,
+		reviews: req.body.reviews
+	}, (err, location) => {
+		if (err) {
+			return res.status(500).json({message: err});
+		} else if (!location) {
+			return res.status(404).json({message: "location not found"});
+		} else {
+			location.save((err, savedLocation) => {
+				if (err) {
+					return res.status(400).json({message: err});
+				} else {
+					return res.status(200).json({message: "location updated successfully"})
+				}
+			});
+		}
+	})
+}
