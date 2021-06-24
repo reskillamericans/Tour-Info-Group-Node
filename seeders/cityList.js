55237945bf7eb9efacf4408e44244a4b2d70ea5e
Ricardo Maya <rickmaya@gmail.com>
2021-06-24 13:45:18 -0500

    feat: moved json files to seeder directory and added calback function to cityList.js

--- seeders/cityList.js
+++ seeders/cityList.js
@@ -1,11 +1,22 @@
 const cityList = require('../models/cities.json');
 
+//add callback functions to the .create functions
+// if city is created...
 function seedCities() {
 	for (var i = 0; i < cityList.length; i++){
 		Location.create({
 			name: cityList[i].name,
 			type: "city"
+		}, (err, createdCity) => {
+			if (err) throw err;
+			// if city already exists
+
+			createdCity.save((err, savedCity) => {
+				if (err) throw err;
+				console.log("City Saved!");
+			})
 		})
 	}
 	console.log('cities seeded successfully');
 }
+
--- seeders/countryList.js
+++ seeders/countryList.js
@@ -2,10 +2,18 @@ const countryList = require('../models/countries.json');
 
 function seedCountries() {
 	for (var i = 0; i < countryList.length; i++){
+		//creat location seeded from country list
 		Location.create({
 			name: countryList[i].name,
 			type: "country"
+		}, (err, createdCountry) => {
+			// callback for createdCountry
+			if (err) throw err;
+			createdCountry.save((err, savedCountry) => {
+				if (err) throw err;
+				console.log("Country Saved!");
+			})
 		})
 	}
-	console.log('countries seeded successfully');
+	console.log('country seeded successfully');
 }
