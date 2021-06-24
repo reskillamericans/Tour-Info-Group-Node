2119accb49dffcfae52d18c9c37ae71f28f4a796
Ricardo Maya <rickmaya@gmail.com>
2021-06-24 14:00:12 -0500

    feat: created conditional callback if location exists and callback to create location

--- seeders/cityList.js
+++ seeders/cityList.js
@@ -4,19 +4,32 @@ const cityList = require('../models/cities.json');
 // if city is created...
 function seedCities() {
 	for (var i = 0; i < cityList.length; i++){
-		Location.create({
-			name: cityList[i].name,
-			type: "city"
-		}, (err, createdCity) => {
-			if (err) throw err;
-			// if city already exists
-
-			createdCity.save((err, savedCity) => {
+		// conditions if location already exists
+		Location.findOne({name: req.body.name}, (err, existingLocation) => {
+			if (err) {
+				return res.status(500).json({err});
+			}
+			if (existingLocation) {
+				return res.status(400).json({message: "this location already exists."})
+			}
+			// create Location
+			Location.create({
+				name: cityList[i].name,
+				type: "city"
+			}, (err, createdCity) => {
+				if (err) throw err;
+			}, createdCity.save((err, savedCity) => {
 				if (err) throw err;
 				console.log("City Saved!");
 			})
 		})
-	}
-	console.log('cities seeded successfully');
-}
+		}
+		console.log('cities seeded successfully');
+	} 
+
+
+
+			// if city already exists
+
+			
 
