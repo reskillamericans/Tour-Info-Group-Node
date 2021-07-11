0ca76afb177625a636c087cfe295b622bcd3b47e
Ricardo Maya <rickmaya@gmail.com>
2021-07-10 19:45:45 -0500

    create controller files to fetch all tours, single tour by id, and start tour booking

--- src/controllers/tourController.js
+++ src/controllers/tourController.js
@@ -1,1 +1,34 @@
-tourController.js
+const Tour = require('../models/tours');
+
+// fetches all tours
+exports.fetchTours = (req, res) => {
+	// search for tours by ...
+	Tour.find((err, tours) => {
+		if (err) {
+			return res.status(500).json({message: err});
+		} else {
+			return res.status(200).json({tours})
+		}
+	})
+}
+
+// fetches single tour
+exports.fetchSingleTour = (req, res) => {
+	// fetches tour by id value
+	Tour.findOne({_id: req.params.id}, (err, tour) => {
+		if (err) {
+			return res.status(500).json({message: err});
+		} else if (!tour) {
+			return res.status(404).json({message: "tour not found"});
+		} else {
+			return res.status(200).json({tour});
+		}
+	})
+}
+
+// book a tour
+exports.bookTour = (req, res) => {
+	Tour.create({
+		
+	})
+}
