c9b9895ca9e14c30113787e662d2f884e52ab3cc
Ricardo Maya <rickmaya@gmail.com>
2021-07-15 18:06:57 -0500

    feat/modify: change file name for user controller file.  create functions in user controller file to return all users, add new user, return specific user, update user, and delete user

---
+++ src/controllers/userController.js
@@ -0,0 +1,101 @@
+const User = require('../models/user');
+const { sendEmail } = require('./passwordController');
+
+// @route GET admin/user
+// @desc Returns all users
+// @access Public
+exports.index = async function (req, res) {
+	const users = await User.find({});
+	res.status(200).json({users});
+}
+
+// @route POST api/user
+// @desc Add a new user
+// @access Public
+exports.store = async (req, res) => {
+	try {
+		const { email } = req.body;
+
+		// Make sure this account doesn't already exist
+		const user = await User.findOne({email});
+
+		if (user) return res.status(401).json({message: 'The email address you have entered is already associated with another account. You can change this users role instead.'});
+
+		// generate a random password
+		const password = '_' + Math.random().toString(36).substr(2, 9); 
+		const newUser = new User({...req.body, password});
+
+		const user_ = await newUser.save();
+
+		// Generate and set password reset token
+		user_.generatePasswordReset();
+
+		// Save the updated user object
+		await user_.save();
+
+		// Get mail options
+		let domain = "http://" + req.headers.host;
+		let subject = "New Account Created";
+		let to = user.email;
+		let from = process.env.SENDER_ADDRESS;
+		let link = "http://" + req.headers.host + "/auth/reset/" + user.resetPasswordToken;
+
+		await sendEmail({to, from, subject});
+
+		res.status(200).json({message: 'An email has been sent to ' + user.email + '.'});
+	} catch (error) {
+		res.status(500).json({success: false, message: error.message})
+	}
+};
+
+// @route GET api/user/{id}
+// @desc Returns a specific user
+// @access Public
+exports.show = async function (req, res) {
+	try {
+		const id = req.params.id;
+		const user = await User.findById(id);
+
+		if (!user) return res.status(401).json({message: 'User does not exist'});
+
+		res.status(200).json({user});
+	} catch (error) {
+		res.status(500).json({message: error.message})
+	}
+};
+
+// @route PUT api/user/{id}
+// @desc Update user details
+// @access Public
+exports.update = async function (req, res) {
+	try {
+		const update = req.body;
+		const id = req.params.id;
+		const userId = req.user._id;
+
+		// Make sure the passed id is that of the logged in user
+		if (userId.toString() !== id.toString()) return res.status(401).json({message: "Sorry, you don't have the permission to update this data."});
+
+		const user = await User.findByIdAndUpdate(id, {$set: update}, {new: true});
+	} catch (error) {
+		res.status(500).json({message: error.message});
+	}
+};
+
+// @route DESTROY api/user/{id}
+// @desc Delete User
+// @access Public
+exports.destroy = async function (req, res) {
+	try {
+		const id = req.params.id;
+		const user_id = req.user.id;
+
+		// Make sure the passed id is that of the logged in user
+		if (user_id.toString() !== id.toString()) return res.status(401).json({message: "Sorry, you don't have the permission to delete this data."});
+
+		await User.findByIdAndDelete(id);
+		res.status(200).json({message: 'User has been deleted'});
+	} catch (error) {
+		res.status(500).json({message: error.message});
+	}
+}
