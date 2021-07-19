0ebe9e233bef1f9ab92afec5ed0869f35b6554ce
Ricardo Maya <rickmaya@gmail.com>
2021-07-19 10:51:53 -0500

    modify: reconfigure tour seeder function

--- src/seeders/tourSeeder.js
+++ src/seeders/tourSeeder.js
@@ -2,21 +2,14 @@ const Tour = require('../models/tours');
 
 
 exports.seedTours = () => {
-	Tour.create({
-		title: req.body.title,
-		descriptions: req.body.descriptions,
-		images: req.body.images,
-		featureImage: req.body.featureImage,
-		notes: req.body.notes,
-		city: req.body.city,
-		country: req.body.country,
-		category: req.body.category,
-		travelType: req.body.travelType
-	}, (err, tour) => {
-		if (err) throw err;
-		tour.save((err, savedTour) => {
-			if (err) throw err;
-			console.log("tours seeded");
-		})
-	})
+	Tour.create((err, createdTours) => {
+		if (err) {
+			console.log(err);
+		} else {
+			createdTours.save((err, savedTours) => {
+				if (err) console.log(err);
+				console.log("tours seeded");
+			});
+		}
+	});
 }
