a53253619b2c3ebb4a3572e2047999fe2bd5217f
TheycallmeNate <nathanmangal@gmail.com>
2021-07-26 21:50:52 -0400

    merged development

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -109,7 +109,7 @@ exports.destroy = async function (req, res) {
 
 exports.profile = async (req, res) => {
   try {
-    const profile = await User.findById({ _id: req.user.id }).populate("tour");
+    const profile = await User.findById({ _id: req.user.id }).populate("bookedTours");
     res.status(200).json({ profile });
   } catch (err) {
     res.status(500).json({ message: error.message });
