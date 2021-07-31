04fede8345992aa851c2cd0f7ad39b52b8ced887
Ricardo Maya <rickmaya@gmail.com>
2021-07-30 19:33:32 -0500

    modify: push newBooking into user booking property and save it. Add await for tour.findByid

--- src/controllers/tourController.js
+++ src/controllers/tourController.js
@@ -48,7 +48,7 @@ exports.bookTour = async (req, res) => {
 		});
 
 		// search for existing tour by id, if no tour exists, create one with logged in user info.
-		Tour.findById(req.body.tour, (err, tour) => {
+		await Tour.findById(req.body.tour, (err, tour) => {
 			if (err) {
 				return res.status(404).json({message: 'Tour not found'});
 			} else {
@@ -61,7 +61,20 @@ exports.bookTour = async (req, res) => {
 					if (err) {
 						return res.status(500).json({ message: err });
 					} else {
-						sendConfirmationEmail(newBooking, req, res);
+						if (loggedInUser.Tours) {
+							loggedInUser.Tours.push(newBooking.tour);
+						} else {
+							loggedInUser.Tours = [newBooking.tour];
+
+							loggedInUser.save((err) => {
+								if (err) {
+									return res.status(500).json({ message: err });
+								} else {
+									sendConfirmationEmail(newBooking);
+									return res.status(200).json({ message: 'Tour booked successfully!', newBooking });
+								}
+							})
+						}
 					}
 				})
 			}
@@ -72,7 +85,7 @@ exports.bookTour = async (req, res) => {
 }
 
 // TODO: abstract make DRY
-async function sendConfirmationEmail(bookedTour, req, res) {
+async function sendConfirmationEmail(bookedTour) {
 	try {
 		// Access user by id and set email variables
 		let userInfo = await User.findById(bookedTour.user, (err, user) => {
@@ -99,9 +112,9 @@ async function sendConfirmationEmail(bookedTour, req, res) {
 		<br><p>If you did not request this, please ignore this email.</p>`;
 
 		await sendMail({to, from, subject, html});
-		res.status(200).json({message: 'An email has been sent to ' + to + '.'});
+		console.log('Email sent');
 	} catch (error) {
-		res.status(500).json({message: error.message})
+		console.log(error);
 	}
 }
 
