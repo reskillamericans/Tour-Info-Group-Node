5089aa7d36df705051c48447e239590c902c9e36
TheycallmeNate <nathanmangal@gmail.com>
2021-08-14 10:48:39 -0400

    setup folder structure for server rendering, test render profile page

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ const path = require("path");
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "../public")));
 
 //==================================================
 // EJS
