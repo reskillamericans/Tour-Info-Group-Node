89348ce1b275affc17e4569dbad79ee3a6c3e3cc
TheycallmeNate <nathanmangal@gmail.com>
2021-08-14 15:43:22 -0400

    add all stylesheets and images

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const app = express();
-const auth = require('./authRoutes');
-const user = require('./userRoutes');
-const authenticate = require('../middlewares/authenticate');
+const auth = require("./authRoutes");
+const user = require("./userRoutes");
+const authenticate = require("../middlewares/authenticate");
 
-app.get('/', (req, res) => {
-	res.status(200).send({ message: "Welcome to the AUTHENTICATION API. Register or Login to test Authentication."});
+router.get("/", (req, res) => {
+  res.status(200).send({ message: "Welcome to the AUTHENTICATION API. Register or Login to test Authentication." });
 });
 
-app.use('/api/auth', auth);
-app.use('/api/user', authenticate, user);
+router.use("/api/auth", auth);
+router.use("/api/user", authenticate, user);
 
 module.exports = app;
